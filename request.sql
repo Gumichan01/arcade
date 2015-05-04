607936860d052bf4bba078310e93f1807f9e9a5f
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-05-04 13:00:49 +0200

    Mise en commentaire des execute

--- request.sql
+++ request.sql
@@ -11,7 +11,7 @@ where dateReservation between '01/01/2014' and '31/12/2014'
 group by idArcade
 order by idArcade ASC;
 
-execute taux_occupation(null);
+--execute taux_occupation(null);
 
 
 -- 2)  Donner les recettes du mois en cours, par type de prestation, en ordre croissant de recettes.
@@ -25,7 +25,7 @@ and extract(month from current_date)=extract(month from dateReservation)
 group by r.prestation
 order by recette_du_mois asc;
 
-execute recette_mois_courant(null);
+--execute recette_mois_courant(null);
 
 
 -- 3) Donner la liste des prestations n’ayant fait l’objet d’aucune réservation le mois dernier.
@@ -40,7 +40,7 @@ select distinct prestation::text from projet.reservation
 where extract(year from current_date)=extract(year from dateReservation)
 and (extract(month from current_date)-1)=(extract(month from dateReservation)-1);
 
-execute prestation_sans_reservation_precedent_mois(null);
+--execute prestation_sans_reservation_precedent_mois(null);
 
 
 -- 4) Quelle a été la journée la plus occupée de chaque semaine, pour les 8 dernières semaines ? Comptez la durée des réservations et non pas le nombre de réservations par jour.
@@ -53,7 +53,7 @@ where dateReservation <= current_date and dateReservation >= (current_date - int
 group by dateReservation
 order by duree_en_heure desc);
 
-execute journée_la_plus_occupée(null);
+--execute journée_la_plus_occupée(null);
 
 
 --select * from projet.reservation where dateReservation='13/04/2015';
@@ -65,7 +65,7 @@ prepare moyenne_jours_en_avance(void) as
 select avg(date_part('day', dateReservation::timestamp - dateDemandeReservation::timestamp)) as moyenne_jours_en_avance_pourcent 
 from projet.reservation;
 
-execute moyenne_jours_en_avance(null);
+--execute moyenne_jours_en_avance(null);
 
 
 -- 6) Pour chaque mois de 2014, identifiez le client ayant le plus dépensé sur le site.
@@ -80,7 +80,7 @@ where r.dateReservation between '01/01/2014' and '31/12/2014'
 group by r.arcade, c.idClient, mois_2014
 order by depense desc;
 
-execute client_depense_mois_2014(null);
+--execute client_depense_mois_2014(null);
 
 
 /* Requetes inventées */
@@ -103,7 +103,7 @@ order by chiffre_affaire desc) p_ca
 on idArcade=p_ca.arcade
 where  p_ca.chiffre_affaire >= capital / 20;
 
-execute requete_inventée_1(null);
+--execute requete_inventée_1(null);
 
 
 -- 2) Quel est le client ayant le plus dépensé en 2014 dont l'adresse de la salle d'arcade est la même que son lieu d'habitation ?
@@ -117,7 +117,7 @@ group by client, arcade
 having (select adresse from projet.client where idClient=client)=(select adresse from projet.salle_arcade where idArcade=arcade)
 order by depense desc;
 
-execute requete_inventée_2(null);
+--execute requete_inventée_2(null);
 
 
 -- 3) Quelle sont les jeux possedés par la salle d'arcade ayant le plus de reservations dans l'année ?
